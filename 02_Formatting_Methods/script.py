7dec1040399d8352507d6829e8e76e99d4dbfc09
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-26 14:18:29 +0200

    stings_methods_02

--- 02_Formatting_Methods/script.py
+++ 02_Formatting_Methods/script.py
@@ -1,2 +1,8 @@
 poem_title = "spring storm"
 poem_author = "William Carlos Williams"
+
+poem_title_fixed = poem_title.title()
+print(poem_title_fixed)
+
+poem_author_fixed = poem_author.upper()
+print(poem_author_fixed)
