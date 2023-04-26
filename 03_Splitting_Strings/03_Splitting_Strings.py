1e20b2949d34c8e352e20fb14a31a3e4801fd5ca
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-26 14:48:17 +0200

    stings_methods_03

--- 03_Splitting_Strings/03_Splitting_Strings.py
+++ 03_Splitting_Strings/03_Splitting_Strings.py
@@ -1,23 +1,27 @@
 """
-
 String Methods
 Splitting Strings
 
-.upper(), .lower(), and .title() all are performed on an existing string and produce a string in return. Let’s take a look at a string method that returns a different object entirely!
+.upper(), .lower(), and .title() all are performed on an existing string and produce a string in return. Let’s take a
+look at a string method that returns a different object entirely!
 
-.split() is performed on a string, takes one argument, and returns a list of substrings found between the given argument (which in the case of .split() is known as the delimiter). The following syntax should be used:
-
+.split() is performed on a string, takes one argument, and returns a list of substrings found between the given argument
+(which in the case of .split() is known as the delimiter). The following syntax should be used:
+"""
+delimiter ="the delimiter"
+string_name = ""
 string_name.split(delimiter)
-
+"""
 If you do not provide an argument for .split() it will default to splitting at spaces.
 
 For example, consider the following strings:
-
+"""
 man_its_a_hot_one = "Like seven inches from the midday sun"
 print(man_its_a_hot_one.split())
 # => ['Like', 'seven', 'inches', 'from', 'the', 'midday', 'sun']
-
-.split returned a list with each word in the string. Important to note: if we run .split() on a string with no spaces, we will get the same string in return.
+"""
+.split returned a list with each word in the string. Important to note: if we run .split() on a string with no spaces,
+we will get the same string in return.
 Instructions
 1.
 
